0ec6810edf194dbe7959ad0697e49000afa19905
developernaimul <developernaimul@gmail.com>
2022-06-24 00:18:29 +0600

    file add

---
+++ app.js
@@ -0,0 +1,48 @@
+/**
+* Array to string conversion 
+-> toString
+-> join
+-> split
+* Array Add & Remove 
+-> push
+-> pop 
+-> shift
+-> unshift 
+-> slice
+-> splice  
+ */
+
+
+let naim = ['alu', 'potol', 'law', 'kumra', 'dheros', 'korola', 'puishakh', 'tomato']
+
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, repudiandae';
+
+
+// naim.push('amader')
+// naim.push('tomra')
+// naim.unshift('1')
+// naim.pop()
+// naim.shift()
+
+
+// let naim2 = naim.slice(6, 7)
+// let naim3 = naim.splice(7,1)
+
+// let naim4 = [...naim.slice(1,2), ...naim.slice(5,6),...naim.splice(1,1)]
+
+// naim.unshift()
+
+
+
+
+
+let naim6 = naim.join(' ')
+let stringToArry = lorem.split(' ');
+
+
+console.log(naim6);
+console.log(stringToArry);
+
+// console.log(naim2);
+// console.log(naim3);
+// console.log(naim4);
